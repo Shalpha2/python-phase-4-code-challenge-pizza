6ae1ed9444a8b3ce1b101831b122d0d9b6c8fced
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-06-22 01:57:38 +0300

    reset

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
+from flask_cors import CORS
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -16,6 +17,7 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+CORS(app)
 
 api = Api(app)
 
